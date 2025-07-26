b9f8e6efe30bc233fa30f5bfc787720d3aa160d9
Sajjad <askari.sajjad.1993@gmail.com>
2025-07-26 12:33:12 +0330

    edit searchParams3

--- src/app/casual/page.tsx
+++ src/app/casual/page.tsx
@@ -7,8 +7,8 @@ import { Ipagination } from "@/type";
 
 interface props{
   // params: {id: string},
-  // searchParams: {page?: string , per_page?: string}
-  searchParams?: Record<string, string | undefined>;
+  searchParams: {page?: string , per_page?: string}
+  // searchParams?: Record<string, string | string[] | undefined>;
 }
 
 
